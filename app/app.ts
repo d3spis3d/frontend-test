0a85726ea25a8aba47c6af033a1c17ff51ec5943
Daniel Budden <daniel.l.budden@gmail.com>
2016-04-03 00:24:27 +1100

    Setup store

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+
+import AppContainer from './components/app-container';
+import rootReducer from './reducers/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+
+ReactDom.render(
+    <Provider store={store}>
+        <AppContainer/>
+    </Provider>,
+    document.getElementById('root')
+);
